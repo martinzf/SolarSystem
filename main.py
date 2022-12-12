b9c2c0c78cdf02aab56327b219a5f850c87a0a4e
Martin Zapata <martzapa@ucm.es>
2022-12-12 10:23:39 +0100

    Better variable naming and some typing

--- main.py
+++ main.py
@@ -7,33 +7,37 @@ from matplotlib.animation import FuncAnimation
 plt.style.use(['dark_background'])
 
 # E.M. Standish (1992)'s data for simplified orbit propagation
-keplerel = pd.read_csv('keplerel.csv', index_col=0)            # Planets' Kepler elements
-jovians = pd.read_csv('jovians.csv', index_col=0)              # Jovian planets' correction terms
+orbitalElements: pd.DataFrame = pd.read_csv('orbitel.csv', index_col=0) 
+joviansCorrection: pd.DataFrame = pd.read_csv('jovians.csv', index_col=0) 
 
 # Change degrees to radians
-keplerel.loc[:, 'I':'d/dt(long.peri.)'] = np.pi / 180 * keplerel.loc[:, 'I':'d/dt(long.peri.)']
-jovians = np.pi / 180 * jovians
+orbitalElements.loc[:, 'I':'d/dt(long.peri.)'] = np.pi / 180 * orbitalElements.loc[:, 'I':'d/dt(long.peri.)']
+joviansCorrection = np.pi / 180 * joviansCorrection
 
 # Classical Kepler orbital elements
-keplerel['w'] = keplerel['long.peri.'] - keplerel['long.node.'] # Argument of perihelion
-keplerel['dw/dt'] = keplerel['d/dt(long.peri.)'] - keplerel['d/dt(long.node.)']
-keplerel['M'] = keplerel['L'] - keplerel['long.peri.']          # Mean anomaly
-keplerel['dM/dt'] = keplerel['dL/dt'] - keplerel['d/dt(long.peri.)']
+# Argument of perihelion
+orbitalElements['w'] = orbitalElements['long.peri.'] - orbitalElements['long.node.'] 
+orbitalElements['dw/dt'] = orbitalElements['d/dt(long.peri.)'] - orbitalElements['d/dt(long.node.)']
+# Mean anomaly
+orbitalElements['M'] = orbitalElements['L'] - orbitalElements['long.peri.'] 
+orbitalElements['dM/dt'] = orbitalElements['dL/dt'] - orbitalElements['d/dt(long.peri.)']
 
-J2000 = dat.date(2000, 1, 1)                                    # Reference epoch (1st July 2000, 12:00 GMT)
-tol = 1.75e-8                                                   # Tolerance for Kepler equation solution (radians)
-ellipsepoints = 50                                              # Orbit line resolution (point count)
-anidpi = 100
-anifps = 50
-aniframes = 150
+J2000: dat.date = dat.date(2000, 1, 1) # Reference epoch (1st July 2000, 12:00 GMT)
+TOL: float = 1.75e-8 # Tolerance for Kepler equation solution (radians)
+ELLIPSEPOINTS: int = 50 # Orbit line resolution (point count)
+ANIDPI: int = 100
+ANIFPS: float = 50
+ANIFRAMES: int = 150
 fig, ax = plt.subplots(figsize=(13, 10), subplot_kw={'projection':'3d'})
-axlims = keplerel['a'].max()
-colours = ['darkgrey', 'darkorange', 'g', 'r', 'darksalmon', 'gold', 'lightblue', 'b']
-lns1 = [ax.plot([], [], [], 'o', label=planet, color=colours[idx])[0] for idx, planet in enumerate(keplerel.index)]
-lns2 = [ax.plot([], [], [], 'k', lw=1)[0] for planet in keplerel.index]
+AXLIMS: float = orbitalElements['a'].max()
+COLOURS: list[str] = ['darkgrey', 'darkorange', 'g', 'r', 'darksalmon', 'gold', 'lightblue', 'b']
+planet_dots = [ax.plot([], [], [], 'o', label=planet, color=COLOURS[idx])[0] 
+    for idx, planet in enumerate(orbitalElements.index)]
+orbit_lines = [ax.plot([], [], [], 'k', lw=1)[0] 
+    for planet in orbitalElements.index]
 time_text = fig.text(.612, .889, '', fontsize=20)
 
-def getdate():
+def start_n_end() -> tuple[dat.date, dat.date]:
     print('Input a date between 3000 BC and 3000 AD')
     # Get terrestrial time (TT), approx. JPL ephemeris time
     while True:
@@ -60,7 +64,7 @@ def getdate():
         end = - ((TT - dat.date(1, 1, 1)) + (J2000 -dat.date(1, 1, 1))).days / 36525
     return start, end
 
-def calcorbit(angles, correction, t):
+def calcorbit(angles: np.array, correction: np.array, t: float) -> tuple[np.array, np.array]:
     # Propagation of Kepler orbital elements using Standish's linear fit
     # Units: centuries, astronomical units, radians
     a, e, w, I, O, M = angles
@@ -75,15 +79,15 @@ def calcorbit(angles, correction, t):
     # Newton's method to find roots of Kepler's equation
     E = M
     f = M - (E - e * np.sin(E))
-    while np.abs(f) > tol:
+    while np.abs(f) > TOL:
         E += f / (1 - e * np.cos(E))
         f = M - (E - e * np.sin(E))
     # Coords. in heliocentric orbital plane (2D)
     x = np.array([[a * (np.cos(E) - e)],
                   [a * np.sqrt(1 - e ** 2) * np.sin(E)]])
-    fullrot = np.linspace(0, 2 * np.pi, ellipsepoints)
-    ellipse = np.vstack((a * (np.cos(fullrot) - e),
-                         a * np.sqrt(1 - e ** 2) * np.sin(fullrot)))
+    two_pi_radians = np.linspace(0, 2 * np.pi, ELLIPSEPOINTS)
+    ellipse = np.vstack((a * (np.cos(two_pi_radians) - e),
+                         a * np.sqrt(1 - e ** 2) * np.sin(two_pi_radians)))
     # Coords. in J2000 ecliptic plane (3D), rotation matrix (Rz(O) * Rx(i) * Rz(w))
     cw, sw = np.cos(w), np.sin(w)
     ci, si = np.cos(I), np.sin(I)
@@ -97,23 +101,23 @@ def calcorbit(angles, correction, t):
 
 def init():
     # Initialise figure
-    ax.set_xlim3d(-axlims, axlims)
-    ax.set_ylim3d(-axlims, axlims)
-    ax.set_zlim3d(-axlims, axlims)
+    ax.set_xlim3d(-AXLIMS, AXLIMS)
+    ax.set_ylim3d(-AXLIMS, AXLIMS)
+    ax.set_zlim3d(-AXLIMS, AXLIMS)
     ax.set_xlabel(r'$\hat{X}$: Vernal Equinox (AU)', labelpad=10, fontsize=15)
     ax.set_ylabel(r'$\hat{Y}=\hat{Z}\times\hat{X}$ (AU)', labelpad=10, fontsize=15)
     ax.set_zlabel(r'$\hat{Z}$: North Ecliptic Pole (AU)', labelpad=10, fontsize=15)
     ax.set_title('Solar System, ECLIPJ2000 reference frame,', fontsize=20)
     plt.subplots_adjust(left=-0.15)
     ax.legend(bbox_to_anchor=(1.5, .8), fontsize=15)
-    return *lns1, *lns2
+    return *planet_dots, *orbit_lines
 
-def animate(t):
-    for planet_name, planet in keplerel.iterrows():
+def animate(t: float):
+    for planet_name, planet in orbitalElements.iterrows():
         # Get coordinates for planets and orbits
-        idx = list(keplerel.index).index(planet_name)
-        if planet_name in jovians.index:
-            correction = jovians.loc[planet_name].to_numpy()
+        idx = list(orbitalElements.index).index(planet_name)
+        if planet_name in joviansCorrection.index:
+            correction = joviansCorrection.loc[planet_name].to_numpy()
         else:
             correction = np.zeros(4)
         elements = planet[['a', 'e', 'w', 'I', 'long.node.', 'M']].to_numpy()
@@ -121,11 +125,11 @@ def animate(t):
         angles = elements + t * variation
         xecl, ellipsecl = calcorbit(angles, correction, t)
         # Plot planets
-        lns1[idx].set_data(xecl[0], xecl[1])
-        lns1[idx].set_3d_properties(xecl[2])
+        planet_dots[idx].set_data(xecl[0], xecl[1])
+        planet_dots[idx].set_3d_properties(xecl[2])
         # Plot orbits
-        lns2[idx].set_data(ellipsecl[0], ellipsecl[1])
-        lns2[idx].set_3d_properties(ellipsecl[2])
+        orbit_lines[idx].set_data(ellipsecl[0], ellipsecl[1])
+        orbit_lines[idx].set_3d_properties(ellipsecl[2])
     # Display date
     try:
         date = J2000 + dat.timedelta(days=t*36525)
@@ -134,15 +138,15 @@ def animate(t):
         date = dat.date(1, 1, 1) - dat.timedelta(days=t*36525) - (J2000 - dat.date(1, 1, 1))
         era = 'BC'
     time_text.set_text(f'{date} {era} (TT)')
-    return *lns1, *lns2
+    return *planet_dots, *orbit_lines
 
 def main():
-    start, end = getdate()
-    frames = np.concatenate((start * np.ones(int(anifps / 2)),  # Linger on starting frame
-                             np.linspace(start, end, aniframes - anifps),
-                             end * np.ones(int(anifps / 2))))   # Linger on final frame
+    start, end = start_n_end()
+    frames = np.concatenate((start * np.ones(int(ANIFPS / 2)),  # Linger on starting frame
+                             np.linspace(start, end, ANIFRAMES - ANIFPS),
+                             end * np.ones(int(ANIFPS / 2))))   # Linger on final frame
     ani = FuncAnimation(fig, animate, frames=frames, init_func=init, interval=50, blit=True)
-    ani.save('solar_system.gif', writer='pillow', fps=anifps, dpi=anidpi)
+    ani.save('solar_system.gif', writer='pillow', fps=ANIFPS, dpi=ANIDPI)
     webbrowser.open('solar_system.gif')
 
 if __name__=="__main__":
